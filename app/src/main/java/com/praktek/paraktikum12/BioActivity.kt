2955fc74b8f064bb5d44f685dca7b03dd30706ca
Lazua <lazuardiahnafekasandhi@gmail.com>
2022-11-10 08:26:27 +0700

    Initial commit

---
+++ app/src/main/java/com/praktek/paraktikum12/BioActivity.kt
@@ -0,0 +1,11 @@
+package com.praktek.paraktikum12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.bio)
+    }
+}
